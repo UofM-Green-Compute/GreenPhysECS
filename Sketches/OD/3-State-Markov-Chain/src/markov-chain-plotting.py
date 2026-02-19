6a18a812974694ea6b2b10cc5dce9c4dc85356be
Oluwole Delano <oluwoledelano@gmail.com>
2026-02-19 21:37:30 +0000

    OD 19/2/26 21:32 -- Updates to the 3 state markov chain (representing the SIR model). Interaction has been added by making the probability of a new infection dependent on the number of currently infected people. This change results in a plot for S, I, and R that has identical shape to the plots produced by Mattias, showing that our two different approaches converge to make the same result.

--- Sketches/OD/3-State-Markov-Chain/src/markov-chain-plotting.py
+++ Sketches/OD/3-State-Markov-Chain/src/markov-chain-plotting.py
@@ -1,7 +1,18 @@
-import numpy 
+import os
+import numpy as np
 import matplotlib.pyplot as plt 
 
-N = 10 
-x = numpy.linspace(0,N,N)
+root_folder = os.path.dirname(os.path.dirname(__file__))
+data_path_SIR = os.path.join(root_folder, "outputs", "SIR.txt")
 
+SIR_count = np.genfromtxt(data_path_SIR, delimiter = ',', skip_header = 1)
 
+figSIR, axSIR = plt.subplots()
+axSIR.plot(SIR_count[:,0],SIR_count[:,1],label='Susceptible')
+axSIR.plot(SIR_count[:,0],SIR_count[:,2],label='Infected')
+axSIR.plot(SIR_count[:,0],SIR_count[:,3],label='Recovered')
+figSIR.legend(loc='upper center', ncol=3) 
+
+figSIR.savefig("SIR")
+
+plt.show()
--- Sketches/OD/3-State-Markov-Chain/src/markov-chain.cpp
+++ Sketches/OD/3-State-Markov-Chain/src/markov-chain.cpp
@@ -6,53 +6,70 @@ Oluwole Delano
  { q_11 | q_12 | q_13 }
  { q_21 | q_22 | q_23 }
  { q_31 | q_32 | q_33 }
-   --> where q_nm = q_n<-m = probability to transition from state n to state m
+   -- where q_nm = q_n<-m = probability to transition from state n to state m
 */
 
 #include <flecs.h>
 #include <iostream>
 #include <fstream>
 #include <random>
+#include <time.h> 
 
-int TIME_STEPS = 50; 
+int TIME_STEPS = 200; 
 
-double Q11 = 2.0 / 3.0;
-double Q21 = 1.0 / 3.0;
+// Counters to keep track of how many people are in each state
+int NO_PEOPLE = 100000; // Total number of people
+int noS = 0; 
+int noI = 0; 
+int noR = 0; 
+// Transition constants
+double beta = 1.0 / static_cast<double>(NO_PEOPLE); 
+double alpha = 0.05; 
+
+// Transition probability functions 
+double calculate_Q21(int nS, int nI, int nR) { return beta * nI; }
+double calculate_Q32(int nS, int nI, int nR) { return alpha; }
+double Q11 = 1 - calculate_Q21(noS, noI, noR);
+double Q22 = 1 - calculate_Q32(noS, noI, noR);
+// Transition probabilities
 double Q31 = 0.0;
-double Q12 = 1.0 / 4.0;
-double Q22 = 1.0 / 2.0;
-double Q32 = 1.0 / 4.0;
-double Q13 = 0.0;
-double Q23 = 0.0;
-double Q33 = 1.0;
+double Q12 = 0.0;
+double Q13 = 0;
+double Q23 = 0;
+double Q33 = 1;
 
-// Initialise State Tags (Note that the sum of qmn for each component must equal 1)
+// Initialise State components (Note that the sum of qmn for each component must equal 1)
+struct PeopleIndex { int p; }; 
 struct state { int s; }; 
-struct state1 { double q11, q21, q31; }; 
-struct state2 { double q12, q22, q32; }; 
-struct state3 { double q13, q23, q33; }; 
+struct state1 { double q11, q21, q31; }; // "Susceptible"
+struct state2 { double q12, q22, q32; }; // "Infected"
+struct state3 { double q13, q23, q33; }; // "Recovered"
 
 int main(int argc, char* argv[]) {
+
+    // Start measuring run time of program
+    clock_t t; 
+    t = clock(); 
 
     // *** Programming setup *** 
 
     // Opening data file 
-    std::ofstream MyFile;
-    MyFile.open("3-state-markov-chain.txt");
+    std::ofstream MyFileSIR;
+    MyFileSIR.open("SIR.txt");
     // Check if file is open
-    if (!MyFile.is_open())
+    if (!MyFileSIR.is_open())
     {
         std::cout<<"Error in creating file"<<std::endl; 
         return 1; 
     }
-    MyFile << "Time step | State" << std::endl; 
-
-    std::cout<<"Hello world I'm a 3 state Markov chain"<<std::endl; 
+    MyFileSIR << "Time step | Susceptible (S) | Infected (I) | Recovered (R)" << std::endl; 
 
     // Initialise flecs world
     flecs::world world(argc,argv);
 
     // Creating components
+    world.component<PeopleIndex>(); 
+    world.component<state>(); 
     world.component<state1>(); 
     world.component<state2>(); 
     world.component<state3>(); 
@@ -63,79 +80,115 @@ int main(int argc, char* argv[]) {
 
     // *** Physics ***
 
-    // Set the initial condition
-    auto counter = world.entity(); 
-    counter.set<state>({1}); 
-    counter.set<state1>({Q11, Q21, Q13}); 
+    // Reserve space for people
+    std::vector<flecs::entity> people; 
+    people.reserve(2*NO_PEOPLE); 
 
-    // Change the state of the system
-    world.system<state1>()
-        .each([&](state1& s1){
-            double rand = dist(rng); 
-            if(rand < s1.q11){
-                std::cout<<"Staying in state 1"<<std::endl; 
-            }
-            else if( (rand > s1.q11) && (rand < (s1.q11 + s1.q21)) ){
-                std::cout<<"Moving to state 2"<<std::endl; 
-                counter.remove<state1>(); 
-                counter.set<state>({2});
-                counter.set<state2>({Q12, Q22, Q32}); 
-            }
-            else if( (rand > (s1.q11 + s1.q21)) && (rand < (s1.q11 + s1.q21 + s1.q31)) ){ 
-                std::cout<<"Moving to state 3"<<std::endl;  
-                counter.remove<state1>();
-                counter.set<state>({3});
-                counter.set<state3>({Q13, Q23, Q33}); 
-            }
-        }); 
-    
-    world.system<state2>()
-        .each([&](state2& s2){
-            double rand = dist(rng); 
-            if(rand < s2.q12){
-                std::cout<<"Moving to state 1"<<std::endl; 
-                counter.remove<state2>(); 
-                counter.set<state>({1}); 
-                counter.set<state1>({Q11, Q21, Q31});
-            }
-            else if( (rand > s2.q12) && (rand < (s2.q12 + s2.q22)) ){
-                std::cout<<"Staying in state 2"<<std::endl; 
-            }
-            else if( (rand > (s2.q12 + s2.q22)) && (rand < (s2.q12 + s2.q22 + s2.q32)) ){
-                std::cout<<"Moving to state 3"<<std::endl; 
-                counter.remove<state2>(); 
-                counter.set<state>({3}); 
-                counter.set<state3>({Q13, Q23, Q33}); 
-            }
-        }); 
+    // Set the initial condition 
+    //  -- Set initial numbers of each population -- 
+    noI = 2; 
+    noS = NO_PEOPLE - noI; 
+    // -- Set the initial transition probabilities -- 
+    double Q21 = calculate_Q21(noS, noI, noR); 
+    double Q32 = calculate_Q32(noS, noI, noR); 
+    double Q11 = 1 - Q21;
+    double Q22 = 1 - Q32;
+    // -- Initialise entities -- 
+    for (int i = 0; i < noS; ++i) { 
+        people.push_back( 
+            world.entity() 
+                .set<PeopleIndex>({i})
+                .set<state>({1})
+                .set<state1>({Q11, Q21, Q31})
+        ); 
+    } 
+    for (int i = 0; i < noI; ++i) { 
+        people.push_back( 
+            world.entity() 
+                .set<PeopleIndex>({noS + i})
+                .set<state>({2})
+                .set<state2>({Q12, Q22, Q32})
+        ); 
+    }  
 
-    world.system<state3>()
-        .each([&](state3& s3){
+    // Update each entity in the world
+    world.system<state3, PeopleIndex>()
+        .each([&](state3& s3, PeopleIndex& person){
             double rand = dist(rng);
             if(rand < s3.q13){
-                std::cout<<"Moving to state 1"<<std::endl; 
-                counter.remove<state3>(); 
-                counter.set<state>({1}); 
-                counter.set<state1>({Q11, Q21, Q31});
+                // Moving to state 1
+                std::cout<<"ERROR: System cannot transition from state 3 to state 1"<<std::endl;
             }
             else if( (rand > s3.q13) && (rand < (s3.q13 + s3.q23)) ){
-                std::cout<<"Moving to state 2"<<std::endl;
-                counter.set<state>({2});
-                counter.remove<state3>(); 
-                counter.set<state2>({Q12, Q22, Q32});  
+                // Moving to state 2
+                std::cout<<"ERROR: System cannot transition from state 3 to state 2"<<std::endl;
             }
             else if( (rand > (s3.q13 + s3.q23)) && (rand < (s3.q13 + s3.q23 + s3.q33)) ){
-                std::cout<<"Staying in state 3"<<std::endl; 
+                // Staying in state 3
+                people[person.p].set<state3>({Q13, Q23, Q33});
             }
-            
+        }); 
+
+    world.system<state2, PeopleIndex>()
+        .each([&](state2& s2, PeopleIndex& person){
+            double rand = dist(rng); 
+            if(rand < s2.q12){
+                // Moving to state 1
+                std::cout<<"ERROR: System cannot transition from state 2 to state 1"<<std::endl;
+            }
+            else if( (rand > s2.q12) && (rand < (s2.q12 + s2.q22)) ){
+                // Staying in state 2
+                double q32 = alpha; 
+                double q22 = 1 - Q32;
+                people[person.p].set<state2>({Q12, q22, q32}); 
+            }
+            else if( (rand > (s2.q12 + s2.q22)) && (rand < (s2.q12 + s2.q22 + s2.q32)) ){
+                // Moving to state 3
+                noI -=1; 
+                noR += 1; 
+                people[person.p].remove<state2>(); 
+                people[person.p].set<state>({3}); 
+                people[person.p].set<state3>({Q13, Q23, Q33}); 
+            }
+        }); 
+
+    world.system<state1, PeopleIndex>()
+        .each([&](state1& s1, PeopleIndex& person){
+            double rand = dist(rng); 
+            if(rand < s1.q11){
+                // Staying in state 1
+                double q21 = beta * noI; 
+                double q11 = 1 - q21;
+                people[person.p].set<state1>({q11, q21, Q31}); 
+            }
+            else if( (rand > s1.q11) && (rand < (s1.q11 + s1.q21)) ){
+                // Moving to state 2
+                noS -= 1; 
+                noI += 1; 
+                double q32 = alpha; 
+                double q22 = 1 - Q32;
+
+                people[person.p].remove<state1>(); 
+                people[person.p].set<state>({2});
+                people[person.p].set<state2>({Q12, q22, q32}); 
+            }
+            else if( (rand > (s1.q11 + s1.q21)) && (rand < (s1.q11 + s1.q21 + s1.q31)) ){ 
+                // Moving to state 3
+                std::cout<<"ERROR: System cannot transition from state 1 to state 3"<<std::endl;
+            }
+
         }); 
 
     // Run the world TIME_STEPS times
     for(int i = 0; i < TIME_STEPS; i++){
 
-        MyFile << i << "," << counter.get<state>().s <<std::endl; 
+        MyFileSIR << i << "," << noS << "," << noI << "," << noR << std::endl; 
         world.progress(); 
 
     }
 
+    t = clock() - t; 
+    double time_taken = ((double)t) / CLOCKS_PER_SEC;
+    std::cout<<"RUN TIME: "<<time_taken<<"s"<<std::endl;
+    
 }
