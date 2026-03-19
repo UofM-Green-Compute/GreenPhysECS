22467d77c103e960776b75280f7b4eb2f3166edc
Mattias Evans <mattiasevans@gmail.com>
2026-03-19 14:55:12 +0000

    started reproducing the model in the paper

---
+++ Sketches/ME/HUDmodel/src/plotting.py
@@ -0,0 +1,46 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+rootFolder = os.path.dirname(os.path.dirname(__file__))
+savePathSIS = os.path.join(rootFolder, "outputs", "SISensemble.pdf")
+
+#susceptible
+susceptibleMeanPath = os.path.join(rootFolder, "outputs", "SusceptibleMean.txt")
+susceptibleUpperPath = os.path.join(rootFolder, "outputs", "SusceptibleUpper.txt")
+susceptibleLowerPath = os.path.join(rootFolder, "outputs", "SusceptibleLower.txt")
+time = np.genfromtxt(susceptibleMeanPath, delimiter = ',', skip_header = 1)[:,0]
+susceptibleMean  = np.genfromtxt(susceptibleMeanPath, delimiter = ',', skip_header = 1)[:,1]
+upperSusceptible  = np.genfromtxt(susceptibleUpperPath, delimiter = ',', skip_header = 1)[:,1]
+lowerSusceptible  = np.genfromtxt(susceptibleLowerPath, delimiter = ',', skip_header = 1)[:,1]
+
+#susceptible
+infectedMeanPath = os.path.join(rootFolder, "outputs", "InfectedMean.txt")
+infectedUpperPath = os.path.join(rootFolder, "outputs", "InfectedUpper.txt")
+infectedLowerPath = os.path.join(rootFolder, "outputs", "InfectedLower.txt")
+infectedMean  = np.genfromtxt(infectedMeanPath, delimiter = ',', skip_header = 1)[:,1]
+upperInfected = np.genfromtxt(infectedUpperPath, delimiter = ',', skip_header = 1)[:,1]
+lowerInfected  = np.genfromtxt(infectedLowerPath, delimiter = ',', skip_header = 1)[:,1]
+
+fig, ax = plt.subplots()
+#susceptible
+ax.plot(time,susceptibleMean,label='Susceptible', color = 'tab:blue')
+ax.plot(time,upperSusceptible, color = 'tab:blue', alpha = 0.5)
+ax.plot(time,lowerSusceptible, color = 'tab:blue', alpha = 0.5)
+ax.fill_between(time, susceptibleMean, upperSusceptible, color = 'tab:blue', alpha = 0.1)
+ax.fill_between(time, susceptibleMean, lowerSusceptible, color = 'tab:blue', alpha = 0.1)
+#infected
+ax.plot(time,infectedMean,label='Infected', color = 'tab:orange')
+ax.plot(time,upperInfected, color = 'tab:orange', alpha = 0.5)
+ax.plot(time,lowerInfected, color = 'tab:orange', alpha = 0.5)
+ax.fill_between(time, infectedMean, upperInfected, color = 'tab:orange', alpha = 0.1)
+ax.fill_between(time, infectedMean, lowerInfected, color = 'tab:orange', alpha = 0.1)
+#axis labels and limits
+ax.set_xlabel("time (days)")
+ax.set_ylabel("population (no. people)")
+ax.set_ylim(0, 100)
+ax.set_xlim(0,8)
+#legend
+fig.legend(loc='upper center', ncol=3)
+fig.savefig(savePathSIS)
+plt.show()
---
+++ Sketches/ME/HUDmodel/src/simulation.cpp
@@ -0,0 +1,93 @@
+#include <flecs.h>
+#include <iostream>
+#include <fstream>
+#include <random>
+#include "markovSystems.h"
+
+// Initialise State components (Note that the sum of qnm for each component must equal 1)
+struct PeopleIndex { int p; }; 
+struct State { int s; }; // Track current state of entity
+struct TransitionProbabilities { std::vector<double> qnm; }; // Transition from state m to state n = 1,2
+
+// Tools for picking random numbers 
+std::mt19937 rng( std::random_device{}()  ) ; 
+std::uniform_real_distribution<double> dist(0,1); 
+
+void setupEntities(flecs::world world, std::vector<flecs::entity> &p, int totalPopulation, 
+    int initialInfected){
+    for (int i = 0; i < totalPopulation-initialInfected; ++i) { 
+        std::vector<double> Qnm = {0, 0}; 
+        p.push_back( 
+            world.entity() 
+                .set<PeopleIndex>({i})
+                .set<State>({1})
+                .set<TransitionProbabilities>({Qnm})
+        ); 
+    } 
+    for (int i = 0; i < initialInfected; ++i) { 
+        std::vector<double> Qnm = {0, 0}; 
+        p.push_back( 
+            world.entity() 
+                .set<PeopleIndex>({totalPopulation-initialInfected + i})
+                .set<State>({2})
+                .set<TransitionProbabilities>({Qnm})
+        ); 
+    }
+}
+
+void setupComponents(flecs::world world){
+    world.component<PeopleIndex>(); 
+    world.component<State>(); 
+    world.component<TransitionProbabilities>();
+}
+
+void setupSystems(flecs::world world, std::vector<int> &population, const double infectionRate,
+    const double recoveryRate, const double step) {
+    updateProbabilities(world, population, infectionRate, recoveryRate, step);
+    transition(world, population);
+}
+
+int simulate(int argc, char* argv[], const double infectionRate, const double recoveryRate, 
+const int totalPopulation, const int initialInfected, const double step, const double totalTime, 
+std::string filename) {
+    /*
+    This function
+    1) Initializes a population vector
+    2) Creates a world
+    3) Sets up the entities, components, and systems
+    4) Runs the simulation
+    5) Saves the data to a data file every time step
+    */
+
+    // Population vector will be updated and saved over time
+    std::vector<int> population_vector = {totalPopulation-initialInfected, initialInfected, 0};
+
+    // Initialise flecs world
+    flecs::world world(argc,argv);
+
+    // Creating components
+    setupComponents(world);
+
+    // Create a people vector and generate entities
+    std::vector<flecs::entity> people; 
+    people.reserve(totalPopulation); 
+    setupEntities(world, people, totalPopulation, initialInfected); 
+
+    // Create the systems
+    setupSystems(world, population_vector, infectionRate, recoveryRate, step);
+
+    // Run the simulation
+    std::ofstream MyFile;
+    MyFile.open(filename);
+    MyFile << "Time,Susceptible,Infected" << std::endl; 
+    double time = 0;
+    MyFile << time << "," << population_vector[0] << "," << population_vector[1] << std::endl;  
+    while (time<totalTime) {
+        world.progress();
+        time += step;
+        // update sample variables
+        MyFile << time << "," << population_vector[0] << "," << population_vector[1] << std::endl;  
+    }
+    MyFile.close();
+    return 0;
+}
