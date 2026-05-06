76b68d263d11c774c1fe7acb122d44147e0a6db2
Oluwole Delano <oluwoledelano@gmail.com>
2026-05-06 22:24:44 +0100

    OD 6/5/26 22:23 - 1) Trying to fix the lattic code so that is uses the maximul amount of space available. Changes are NOT functional, will either continue working on it or scrap completely.

--- Sketches/OD/Lattice-Drawing/src/main.cpp
+++ Sketches/OD/Lattice-Drawing/src/main.cpp
@@ -9,7 +9,7 @@
 double X_LIM = 1;
 double Y_LIM = 1;
 double C = 1; // Ratio A / B
-double PSI =  M_PI / 2; // Allowed values: 0 <= PSI <= PI (radians)
+double PSI = 2 * M_PI / 3; // Allowed values: 0 <= PSI <= PI (radians)
 double epsilon = 1 * pow(10,-12); 
 double RADIUS = 0.1 + epsilon; 
 
@@ -17,7 +17,154 @@ double RADIUS = 0.1 + epsilon;
 std::vector<int> TOTAL_NUMBER = {100, 5}; // total number of plants {crops,sentinels}
 
 // Recursive function to setup the lattice 
-std::vector<std::vector<double>> setupLattice(int totalPopulation, std::string filename, std::string filename2, double a, int worked_counter){
+std::vector<std::vector<double>> setupLattice(int totalPopulation, std::string filename, std::string filename2, int no_columns){
+    
+    // Open files 
+    std::ofstream MyFile; 
+    MyFile.open(filename);
+    std::ofstream MyFile2; 
+    MyFile2.open(filename2);
+
+    // Create a vector to store the lattice positions
+    std::vector<std::vector<double>> GridPoints; 
+
+    // Calculate lattice parameters
+    double a = X_LIM / (double(no_columns)-1); 
+    double b = C * a; 
+
+    // Define terms for later use 
+    int backtrack = 0; 
+    double x = 0; 
+    double y = 0; 
+
+    int count = 0; 
+    int j = 0; 
+    while ( (y >= 0) && (y <= Y_LIM) ){
+
+        // Find a valid x position for a row 
+        if(PSI > M_PI / 2) { x = j * b * -cos(PSI); }
+        else{ x = j * b * cos(PSI); } 
+
+        // Set the value of y
+        y = j * b * sin(PSI);
+
+        // Backtrack to the first points within the limits on that row
+        while (x>=-epsilon) {
+            x-=a;
+        }
+        x+=a;
+
+        // Loop through values of x on that row and store data
+        while( (x >= -epsilon) && x <= (X_LIM) && (count < totalPopulation) ){
+
+            MyFile << x << "," << y << "|" ;  
+            MyFile2 << "\\node (" << count << ") " << "at (" << x << "," << y << ") {};" << std::endl;
+            GridPoints.push_back({x,y}); 
+
+            count += 1; 
+            if( x > (X_LIM-a) ) { break; }
+            x+=a; 
+
+        }
+
+        MyFile << std::endl; 
+        if( y > (Y_LIM-(b * sin(PSI))) ) { break; }
+        j+=1; 
+
+    }
+
+    MyFile.close(); 
+    MyFile2.close(); 
+
+    // Base case 
+    if(count == totalPopulation){ return GridPoints; } 
+
+    // Recursive call 
+    return setupLattice(totalPopulation, filename, filename2, no_columns+1); 
+}
+
+// Recursive function to setup the lattice 
+std::vector<double> latticeConstants(int totalPopulation, std::string filename, std::string filename2, int no_columns){
+    
+    // Open files 
+    std::ofstream MyFile; 
+    MyFile.open(filename);
+    std::ofstream MyFile2; 
+    MyFile2.open(filename2);
+
+    // Calculate lattice parameters
+    double a = X_LIM / (double(no_columns)-1); 
+    double b = C * a; 
+
+    // Create a vector to store the lattice positions
+    std::vector<double> consts = {a,b}; 
+
+    // Define terms for later use 
+    int backtrack = 0; 
+    double x = 0; 
+    double y = 0; 
+
+    int count = 0; 
+    int j = 0; 
+    while ( (y >= 0) && (y <= Y_LIM) ){
+
+        // Find a valid x position for a row 
+        if(PSI > M_PI / 2) { x = j * b * -cos(PSI); }
+        else{ x = j * b * cos(PSI); } 
+
+        // Set the value of y
+        y = j * b * sin(PSI);
+
+        // Backtrack to the first points within the limits on that row
+        while (x>=-epsilon) {
+            x-=a;
+        }
+        x+=a;
+
+        // Loop through values of x on that row and store data
+        while( (x >= -epsilon) && x <= (X_LIM) && (count < totalPopulation) ){
+
+            count += 1; 
+            if( x > (X_LIM-a) ) { break; }
+            x+=a; 
+
+        }
+
+        MyFile << std::endl; 
+        if( y > (Y_LIM-(b * sin(PSI))) ) { break; }
+        j+=1; 
+
+    }
+
+    MyFile.close(); 
+    MyFile2.close(); 
+
+    // Base case 
+    if(count == totalPopulation){ return consts; } 
+
+    // Recursive call 
+    return latticeConstants(totalPopulation, filename, filename2, no_columns+1); 
+}
+
+// Modify lattice
+std::vector<std::vector<double>> modifiedLattice(std::vector<std::vector<double>> currentGrid, int totalPopulation, std::string filename, std::string filename2, int no_columns) {
+    double max_y = 0; 
+    double diff = 0; 
+    
+    // Find the maximum y value 
+    for(int i = 0; i < currentGrid.size(); i++){
+        if(currentGrid[i][1] > max_y) { max_y = currentGrid[i][1]; }
+    }
+
+    // 
+    diff = Y_LIM - max_y; 
+
+    std::cout<<"Okay "<<latticeConstants(totalPopulation, filename, filename2, no_columns)[0]<<std::endl; 
+    std::cout<<"Okay 2 "<<latticeConstants(totalPopulation, filename, filename2, no_columns)[1]<<std::endl; 
+}
+
+// Recursive function to setup the lattice 
+std::vector<std::vector<double>> setupLattice(int totalPopulation, std::string filename, std::string filename2, double a){
     
     // Open files 
     std::ofstream MyFile; 
@@ -38,7 +185,6 @@ std::vector<std::vector<double>> setupLattice(int totalPopulation, std::string f
     double y = 0; 
 
     int count = 0; 
-    int count2 = 0; 
     int j = 0; 
     while ( (y >= 0) && (y <= Y_LIM) ){
 
@@ -58,17 +204,14 @@ std::vector<std::vector<double>> setupLattice(int totalPopulation, std::string f
         // Loop through values of x on that row and store data
         while( (x >= -epsilon) && x <= (X_LIM) ){
 
-            // Only write the required no of points 
-            if(count2 < totalPopulation){
-                MyFile << x << "," << y << "|" ;  
-                MyFile2 << "\\node (" << count << ") " << "at (" << x << "," << y << ") {};" << std::endl;
-                GridPoints.push_back({x,y}); 
-                count2+=1; 
-            }
+            MyFile << x << "," << y << "|" ;  
+            MyFile2 << "\\node (" << count << ") " << "at (" << x << "," << y << ") {};" << std::endl;
+            GridPoints.push_back({x,y}); 
 
             count += 1; 
             if( x > (X_LIM-a) ) { break; }
             x+=a; 
+
         }
 
         MyFile << std::endl; 
@@ -81,10 +224,9 @@ std::vector<std::vector<double>> setupLattice(int totalPopulation, std::string f
     MyFile2.close(); 
 
     // Base case 
-    //if (count == totalPopulation){ return GridPoints; } 
-    if (count < totalPopulation){ return setupLattice(totalPopulation, filename, filename2, 0.5 * a, worked_counter); } 
-    if (count >= totalPopulation && worked_counter < 1500){ worked_counter+=1; return setupLattice(totalPopulation, filename, filename2, 1.5 * a, worked_counter); }
-    else{ return GridPoints; }
+    if (count == totalPopulation){ return GridPoints; } 
+    if (count < totalPopulation){ return setupLattice(totalPopulation, filename, filename2, 0.5 * a); } 
+    if (count > totalPopulation){ return setupLattice(totalPopulation, filename, filename2, 1.5 * a); }
 
 }
 
@@ -139,7 +281,10 @@ int main(int argc, char* argv[]) {
     double totalPlants = TOTAL_NUMBER[0] + TOTAL_NUMBER[1];
 
     // Draw the lattice
-    std::vector<std::vector<double>> GridPoints = setupLattice(totalPlants, FILENAME, FILENAME2, X_LIM, 0); 
+    std::vector<std::vector<double>> GridPoints = setupLattice(totalPlants, FILENAME, FILENAME2, X_LIM); 
+
+    // Trying stuff
+    modifiedLattice(GridPoints, totalPlants, FILENAME, FILENAME2, X_LIM); 
 
     // Write the edges 
     setupEdges(GridPoints, FILENAME3); 
